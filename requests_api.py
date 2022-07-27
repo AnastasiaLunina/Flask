d51a7f15d51959e7fba245eea456ec72d1966397
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-07-27 00:49:20 -0400

    Initial commit

---
+++ requests_api.py
@@ -0,0 +1,23 @@
+import requests
+
+HOST = 'http://127.0.0.1:8000'
+
+
+def post():
+    post_data = {
+        'title': 'Selling an aquarium fish',
+        'description': 'Guppi fish, really cute',
+        'author': 'Mike'
+    }
+    response = requests.post(f'{HOST}/advertisements', json=post_data)
+    print(response.json())
+
+
+def get():
+    response = requests.get(f'{HOST}/advertisement/1')
+    print(response.json())
+
+
+def delete():
+    response = requests.delete(f'{HOST}/advertisements/1')
+    print(response.json())
